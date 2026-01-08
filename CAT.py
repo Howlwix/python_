a49ed2c305fc9dfa2194d77e74f8048288391468
Howlwix <maka135421@gmail.com>
2026-01-08 16:23:00 +0700

    Game

--- CAT.py
+++ CAT.py
@@ -1,15 +1,18 @@
 class Cat:
-    def __init__(self, name = str, color = str, meow = str, eat = str):
+    def __init__(self, name = str, color = str, energy = int):
+        self.energy = energy
         self.name = name
         self.color = color
-        self.meow = meow
-        self.eat = eat
-    def is_meow(self):
-        return self.meow
-    def is_eat(self):
-        return self.eat
-cat1 = Cat("red", "white", "meow", "not eat")
-cat2 = Cat("blue", "black", "not meow", "eat")
-cat3 = Cat("green", "white", "not meow", " not eat")
-print(cat3.is_meow())
-print(cat2.is_eat())
+    def meow(self):
+        return "Meow"
+    def eat(self):
+        if self.energy < 100:
+            self.energy += 100-self.energy
+            return "Eat"
+        else:
+            return "Not Eat"
+cat1 = Cat("red", "white",50)
+cat2 = Cat("blue", "black",100)
+cat3 = Cat("green", "white",40)
+print(cat3.meow())
+print(cat3.energy)
